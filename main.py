ed2872355f25d4f49940e2ecbd5d5caa681598fb
Aayush Raghuvamshi <aayush.raghuvamshi@gmail.com>
2023-11-12 14:59:39 -0800

    Got a working flask app that takes requests and returns text generated by gpt2

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hi")
+from flask import Flask, request, jsonify
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+app = Flask(__name__)
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+
+@app.route('/generate', methods=['POST'])
+def generate_text():
+    data = request.json
+    inputs = tokenizer.encode(data['text'], return_tensors='pt')
+    outputs = model.generate(
+        inputs,
+        max_length=100,
+        num_return_sequences=1,
+        temperature=0.7,
+        no_repeat_ngram_size=2
+    )
+
+    text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return jsonify(text=text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
